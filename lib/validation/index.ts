82ee933a5032bb9cc40a9311898076f92f55dd5a
Nitin Awari <nitinawari.dev@gmail.com>
2025-09-20 23:27:54 +0530

    feat(auth): validation schema for register/login using zod

---
+++ lib/validation/index.ts
@@ -0,0 +1,12 @@
+import * as z from "zod";
+
+export const RegisterSchema = z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.email({message:"Invalid email address"}),
+    password:z.string().min(6, "Password must be 6 character long")
+})
+
+export const LoginSchema = z.object({
+    email: z.email({message:"Invalid email address"}),
+    password:z.string().min(1, {message: "password is required"})
+})

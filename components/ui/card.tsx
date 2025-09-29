74341997c62df87efc65c00117dd8d3a7d4c1b2b
Nitin Awari <nitinawari.dev@gmail.com>
2025-09-30 02:19:50 +0530

    fix:use parentheses for implicitly return

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,39 +1,39 @@
 import React from "react"
 
-interface cardProps {
+interface CardProps {
     children: React.ReactNode;
-    className?: string
+    className?: string;
 }
 
 
-const card = ({ children, className }: cardProps) => {
+const Card = ({ children, className }: CardProps) => (
     <div className={`rounded-xl shadow-xl bg-white ${className}`}>
         {children}
     </div>
-}
+)
 
-const cardHeader = ({ children, className }: cardProps) => {
+const CardHeader = ({ children, className }: CardProps) => (
     <div className={`p-6 flex flex-col ${className}`}>
         {children}
     </div>
-}
+)
 
-const cardContent = ({ children, className }: cardProps) => {
+const CardContent = ({ children, className }: CardProps) => (
     <div className={`font-bold text-lg mb-2 ${className}`}>
         {children}
     </div>
-}
+)
 
-const cardFooter = ({ children, className }: cardProps) => {
+const CardFooter = ({ children, className }: CardProps) => (
     <div className={`m-5 ${className}`}>
         {children}
 
     </div>
-}
+)
 
 export {
-    card,
-    cardHeader,
-    cardContent,
-    cardFooter
+    Card,
+    CardHeader,
+    CardContent,
+    CardFooter
 }

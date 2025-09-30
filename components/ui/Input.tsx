be08b541cabfb7d11287d23e1b1a18ee06f163b1
Nitin Awari <nitinawari.dev@gmail.com>
2025-10-01 00:38:26 +0530

    feat: Add Input component with default styling

---
+++ components/ui/Input.tsx
@@ -0,0 +1,12 @@
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  
+}
+
+export const Input = ({ className, ...props}: InputProps) => {
+  return (
+    <input
+      className={`h-9 w-full px-3 py-1 rounded-md border border-gray-200 hover:border-gray-300 text-sm bg-transparent shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-300 ${className}`}
+      {...props}
+    />
+  );
+};
